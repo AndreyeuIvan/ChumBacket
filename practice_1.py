17036251594f10846c352358d0a105d736b1d2f2
AndreyeuIvan <andreeviv@icloud.com>
2020-07-04 23:44:27 +0300

    daily practise with sololearn

---
+++ practice_1.py
@@ -0,0 +1,9 @@
+'''daily practise with sololearn'''
+a = ()
+def f(n):
+    k = ()
+    k = k+ n
+    return k
+for i in range(0,5,2):
+    a += f((i,i+1))
+print(len(a))
--- roman_counter.py
+++ roman_counter.py
@@ -8,40 +8,38 @@ D          500
 M          1,000'''
 
 def solution(number:int):
-    sentence = ''
-    while number >= 1000:
-        number -= 1000
-        sentence += 'M'
-        
-    while number >= 500:
-        number -= 500
-        sentence += 'D'
-
-    while number >= 100:
-        number -= 100
-        sentence += 'C'
-
-    while number >= 50:
-        number -= 50
-        sentence += 'L'
-        
-    while number >= 10:
-        number -= 10
-        sentence += 'X'
-
-    if number == 9:
-        sentence += 'IX'
-    else:
-        while number >= 5:
-            number -= 5
-            sentence += 'V'
-
-    while number >= 1:
-        number -= 1
-        sentence += 'I'
+    sentence = str()
+    roman = {
+        1 : 'I',
+        4 : 'IV',
+        5 : 'V',
+        9 : 'IX',
+        10 : 'X',
+        40 : 'XL',
+        50 : 'L',
+        90 : 'LC',
+        100 : 'C',
+        400 : 'CD',
+        500 : 'D',
+        900 : 'DM',
+        1000 : 'M'
+        }
     
+    while lengh_of_number == 4:
+        number -= 1000
+        sentence += roman.get(1000)
+    while len(str(number)) == 3:
+        if number // 900 == 1:
+            number -= 900
+            sentence += roman.get(900)
+        elif number >= 500:
+            number -= 500
+            sentence += roman.get(500)
+    while len(str(number)) == 2:
+        pass
+    else:
+        pass
+    return sentence, number, lenght_of_number
 
-    return sentence, number
-
-print(solution(1669))
+print(solution(1000))
 
