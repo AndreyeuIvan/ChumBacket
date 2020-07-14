eaecb3124e1482735a254404eb3f8be2e24780be
AndreyeuIvan <andreeviv@icloud.com>
2020-07-14 22:54:38 +0300

    daily practice

---
+++ google_fu.py
@@ -0,0 +1,8 @@
+n = 5
+# Test one.
+if n == 5:
+    print('Yay!')
+
+# Test two.
+if n is 5:
+    print('Yay!')
--- practice_1.py
+++ practice_1.py
@@ -7,3 +7,17 @@ def f(n):
 for i in range(0,5,2):
     a += f((i,i+1))
 print(len(a))
+
+a = b =1
+b = 2
+print(a,b)
+
+l = [1,3,5,7,8]
+for num in l:
+    if num%2 == 0:
+        print(num)
+
+arr = {0,1,2,3,4}
+x = arr[len(arr)-1]
+y = x%4
+print(y)
